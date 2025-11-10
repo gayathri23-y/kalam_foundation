a0d46d4a6e78a0f5e2d6fa8c4fcdf4d461364ed6
gayathri23-y <gayudecember23@gmail.com>
2025-11-10 13:39:14 +0530

    update

--- src/Components/Kalam/Who.jsx
+++ src/Components/Kalam/Who.jsx
@@ -22,7 +22,7 @@ const WhoWeAre = () => {
 
           {/* Center section */}
           <div className="md:col-span-6">
-            <p className="text-black text-base md:text-2xl leading-7 md:leading-9 max-w-prose ml-9">
+            <p className="text-black text-base md:text-xl leading-7 md:leading-9 max-w-md ml-9">
               Pixla Kalam Foundation is a non-profit <br />
                organization dedicated to empowering <br />
                communities, nurturing youth talent, and <br />
